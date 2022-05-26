6ef4971d6269cea6fe4df6e4cf31ef8764619695
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-05-26 21:48:32 +0530

    Fix typing for path/strings

--- engage/engage/doctype/problem_repository/typing_fun.py
+++ engage/engage/doctype/problem_repository/typing_fun.py
@@ -5,6 +5,9 @@ class _SingleOrList:
     def __getitem__(self, typ):
         return Union[typ, List[typ]]
 
+
 # to patch __getitem__, we need an object of a custom class
 # (otherwise, we'd be performing [] on an object of type 'type')
 SingleOrList = _SingleOrList()
+
+PathString = Union[bytes, str, os.PathLike]
--- engage/engage/doctype/problem_repository/util.py
+++ engage/engage/doctype/problem_repository/util.py
@@ -1,37 +1,44 @@
-from os import PathLike
 from pathlib import Path
 from typing import List, Optional
 
 import git
 
-from .typing_fun import SingleOrList
+from .typing_fun import SingleOrList, PathString
 
 
 class ParsedProblem:
     def __init__(
             self,
-            base_path: PathLike,
+            base_path: PathString,
             commit_version: str,
             title: str,
             blurb: str,
-            code: SingleOrList[PathLike],
-            test: SingleOrList[PathLike],
-            files: List[PathLike],
+            files: Dict[str, SingleOrList[PathString]],
             slug: Optional[str] = None,
-            solution: Optional[PathLike] = None,
-            description: Optional[PathLike] = None):
+            description_file: Optional[str] = "description.md"):
 
         self.base_path = Path(path)  # path to problem's base dir
         self.commit_version = commit_version
-        self.title = title
-        self.blurb = blurb
-        self.code = [self.base_path / Path(f_path) for f_path in code]
-        self.files = [self.base_path / Path(f_path) for f_path in files]
-        self.test = [self.base_path / Path(f_path) for f_path in test]
 
         self.slug = slug or self.base_path.name
-        self.solution = self.base_path / (solution or "solution/")
-        self.description = self.base_path / (description or "description.md")
+        self.title = title
+        self.blurb = blurb
+        self.description_file = self.base_path / description_file
+
+        # defaults
+        self.files = {
+            "code": [],
+            "data": [],
+            "test": [],
+            "solution": [],
+        }
+
+        # update from args
+        self.files.update(files)
+
+        # convert PathLike to pathlib.Path
+        for key in ["code", "data", "test", "solution"]:
+            self.files[key] = [self.base_path / Path(f_path) for f_path in code]
 
 
 def shallow_clone(git_url: str, dir_path: PathLike) -> git.Repo:
