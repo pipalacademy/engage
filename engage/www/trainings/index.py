02c7773515af1becfe1b8e9adb036d2151f59800
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-06-13 15:17:11 +0530

    Same trainings listing page for both trainers and students

--- engage/www/trainings/index.py
+++ engage/www/trainings/index.py
@@ -10,17 +10,32 @@ class Status:
 
 
 def get_context(context):
-    trainings = get_trainings_by_trainer(frappe.session.user)
+    trainings = unique(get_trainings_by_trainer(frappe.session.user) + get_trainings_by_participant(frappe.session.user))
+    trainings.sort(key=lambda t: t.begin_date)
+
     trainings_by_status = split_trainings_by_status(trainings)
 
     context.trainings = trainings_by_status
 
 
-def get_trainings_by_trainer(trainer, show_archived=False):
+def get_trainings_by_trainer(trainer):
     """Get trainings by trainer's user ID (i.e. email)"""
 
     training_names = frappe.get_all(
         "Training Trainer",
+        filters={"user": frappe.session.user, "parenttype": "Training"},
+        pluck="parent")
+
+    trainings = [frappe.get_doc("Training", training_name) for training_name in training_names]
+    for t in trainings:
+        t.as_trainer = True
+
+    return trainings
+
+
+def get_trainings_by_participant(participant):
+    training_names = frappe.get_all(
+        "Training Participant",
         filters={"user": frappe.session.user, "parenttype": "Training"},
         pluck="parent")
 
@@ -36,3 +51,7 @@ def split_trainings_by_status(trainings):
         result[training.status].append(training)
 
     return result
+
+
+def unique(ls):
+    return list(set(ls))
