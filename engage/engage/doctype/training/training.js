f02ded224cc7ad4565939155864faddc67b5fd01
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-06-13 16:53:50 +0530

    add trainer to participants automatically for role-access

--- engage/engage/doctype/training/training.js
+++ engage/engage/doctype/training/training.js
@@ -2,7 +2,36 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Training', {
-	// refresh: function(frm) {
+  // refresh: function(frm) {
+    // below code adds current user as a trainer and participant by default
+    // 
+    // if (frm.is_new()) {
+    //   let default_participant = frm.add_child('participants', {
+    //     user: frappe.session.user
+    //   });
 
-	// }
+    //   let default_trainer = frm.add_child('trainers', {
+    //     user: frappe.session.user
+    //   });
+
+    //   frm.refresh_field('participants');
+    //   frm.refresh_field('trainers');
+    // }
+  // }
 });
+
+// frappe.ui.form.on('Training Trainer', {
+//   trainers_add: function(frm, cdt, cdn) {
+//     // when a trainer is added, make an entry for the trainer in the participants table too
+//     // cdt is child doctype, cdn is child docname
+// 
+//     frappe.db.get_doc(cdt, cdn)
+//       .then(trainer => {
+// 	frm.add_child('participants', {
+// 	  user: trainer.user,
+// 	})
+// 
+// 	frm.refresh_field('participants');
+//       });
+//   }
+// });
--- engage/engage/doctype/training/training.py
+++ engage/engage/doctype/training/training.py
@@ -17,7 +17,24 @@ class Training(Document):
         if not isinstance(user, str):
             user = user.name
 
-        return bool(filter(lambda t: t.user == user, self.trainers))
+        return any(filter(lambda t: t.user == user, self.trainers))
+
+    def has_user_as_participant(self, user):
+        """Returns a boolean for whether the given user is a participant for this training or not
+        """
+        if not isinstance(user, str):
+            user = user.name
+
+        return any(filter(lambda p: p.user == user, self.participants))
+
+    def before_save(self):
+        # add trainers as participants, so that they get JupyterHub credentials and
+        # access to web pages that students can access
+        for t in self.trainers:
+            if not self.has_user_as_participant(t.user):
+                self.append("participants", {
+                    "user": t.user,
+                })
 
 
 def slugify(s):
