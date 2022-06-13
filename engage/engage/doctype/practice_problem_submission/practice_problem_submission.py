418c888484eedf9620f7de0154269ff1834c188b
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-06-13 17:24:12 +0530

    Add Practice Problem Latest Submission doctype

--- engage/engage/doctype/practice_problem_submission/practice_problem_submission.py
+++ engage/engage/doctype/practice_problem_submission/practice_problem_submission.py
@@ -1,8 +1,24 @@
 # Copyright (c) 2022, Pipal Academy and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class PracticeProblemSubmission(Document):
-	pass
+    def after_save(self):
+        q = {
+            "training": self.training,
+            "author": self.author,
+            "problem_set": self.problem_set,
+            "problem": self.problem
+        }
+        names = frappe.get_all("Practice Problem Latest Submission", filters=q)
+        if names:
+            latest = frappe.get_doc("Practice Problem Latest Submission", names[0])
+            latest.code = self.code
+            latest.latest_submission = self.name
+            latest.save(ignore_permissions=True)
+        else:
+            data = dict(q, doctype="Practice Problem Latest Submission", latest_submission=self.name, code=self.code)
+            latest = frappe.get_doc(data)
+            latest.insert(ignore_permissions=True)
