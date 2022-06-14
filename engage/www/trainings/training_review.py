3877796c6069ff11ca0a56e753558bf5d274d428
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-06-14 19:58:29 +0530

    feat: styling fixes and leaderboard for training review page

--- engage/www/trainings/training_review.py
+++ engage/www/trainings/training_review.py
@@ -1,6 +1,5 @@
 import frappe
 
-
 NOT_FOUND_TEMPLATE = "www/404.html"
 
 
@@ -15,26 +14,52 @@ def get_context(context):
     username = frappe.form_dict.get("p")
 
     t = get_training(year, slug)
-    if not t or not t.has_user_as_trainer(frappe.session.user):
+    if not (t and t.has_user_as_trainer(frappe.session.user)):
         context.template = NOT_FOUND_TEMPLATE
         return
+
+    latest_submissions = frappe.get_list(
+        "Practice Problem Latest Submission",
+        filters={"training": t.name},
+        fields=["author", "problem", "code"],
+        page_length=10000,
+    )
+    solved_by_user = {}
+    for sub in latest_submissions:
+        solved_by_user.setdefault(sub["author"], {})
+        solved_by_user[sub["author"]][sub["problem"]] = sub
 
     participants = t.participants
     for p in participants:
         p.full_name = frappe.get_cached_doc("User", p.user).full_name
+        p.num_solved = len(solved_by_user.get(p.user, {}))
+        p.active = (p.user == username)
+
+    participants.sort(key=lambda p: p.num_solved, reverse=True)
 
     trainers = t.trainers
     for trainer in trainers:
-        trainer.full_name = frappe.get_cached_doc("User", trainer.user).full_name
+        trainer.full_name = frappe.get_cached_doc("User",
+                                                  trainer.user).full_name
 
-    problem_sets = [frappe.get_cached_doc("Problem Set", row.problem_set) for row in t.problem_sets]
+    problem_sets = [
+        frappe.get_cached_doc("Problem Set", row.problem_set)
+        for row in t.problem_sets
+    ]
 
     for pset in problem_sets:
-        pset.problems = [frappe.get_doc("Practice Problem", p.problem) for p in pset.problems]
+        pset.problems = [
+            frappe.get_doc("Practice Problem", p.problem)
+            for p in pset.problems
+        ]
 
         for problem in pset.problems:
-            latest_submission = username and get_latest_submission(username, t.name, pset.name, problem.name)
-            problem.code = latest_submission and latest_submission.code or problem.code_files[0].content
+            if username:
+                problem.code = solved_by_user.get(username,
+                                                  {}).get(problem.name,
+                                                          {}).get("code", "")
+            else:
+                problem.code = get_starter_code(problem)
 
     context.t = t
     context.title = t.title
@@ -62,10 +87,21 @@ def get_children(child_doctype, parent_name, parent_doctype=None, fields="*"):
 
 
 def get_latest_submission(author, training, problem_set, problem):
-    doc_list = frappe.get_list("Practice Problem Latest Submission",
-            filters={"author": author, "training": training, "problem_set": problem_set, "problem": problem},
-            fields="*",
-            page_length=1)
+    doc_list = frappe.get_list(
+        "Practice Problem Latest Submission",
+        filters={
+            "author": author,
+            "training": training,
+            "problem_set": problem_set,
+            "problem": problem,
+        },
+        fields="*",
+        page_length=1,
+    )
 
     latest = doc_list and doc_list[0] or None
     return latest
+
+
+def get_starter_code(problem):
+    return problem.code_files[0].content
