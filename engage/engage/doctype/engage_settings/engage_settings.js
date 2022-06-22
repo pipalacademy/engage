cfe97820ddebc353c600be509de32defd10c8563
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-06-22 19:56:33 +0000

    Add 'Engage Settings' single doctype

---
+++ engage/engage/doctype/engage_settings/engage_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Pipal Academy and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Engage Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
