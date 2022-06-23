dafd6dd94900332665c86c559a39d8769bfbff6c
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-06-23 16:38:07 +0530

    set route for training invite page

--- engage/hooks.py
+++ engage/hooks.py
@@ -193,6 +193,7 @@ web_include_js = "/assets/engage/js/engage.js"
 website_route_rules = [
     {"from_route": "/trainings/<int:year>/<slug>", "to_route": "trainings/training"},
     {"from_route": "/trainings/<int:year>/<slug>/review", "to_route": "trainings/training_review"},
+    {"from_route": "/trainings/<int:year>/<slug>/invite", "to_route": "trainings/invite"},
     {"from_route": "/trainings/<int:year>/<training>/problems/<problem_set>/<problem>", "to_route": "trainings/problems/problem"},
     {"from_route": "/trainings/<int:year>/<training>/problems/<problem_set>/<problem_repository>/<problem>", "to_route": "trainings/problems/problem"}
 ]
