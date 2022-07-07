e578d37bb82e33c544e71a1a4c480568cbca6eba
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-07-07 16:15:44 +0000

    Show problem/problem-set title instead of slugs

--- engage/www/trainings/review/index.py
+++ engage/www/trainings/review/index.py
@@ -8,7 +8,11 @@ from engage.utils import require_login, require_trainer_role, with_training
 @require_trainer_role
 def get_context(context, training):
     context.training = training
-    context.submissions = frappe.get_all(
-        "Practice Problem Latest Submission",
-        fields=["name", "author", "author_full_name", "problem", "code"],
-        filters={"for_review": True})
+    context.submissions = frappe.get_all("Practice Problem Latest Submission",
+                                         fields=[
+                                             "name", "author",
+                                             "author_full_name",
+                                             "problem_set_title", "problem",
+                                             "problem_title", "code"
+                                         ],
+                                         filters={"for_review": True})
