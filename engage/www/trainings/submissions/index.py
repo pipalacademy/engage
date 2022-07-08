fed1ee369ba1b567914cd0602c8b4be9426edd72
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-07-08 11:50:07 +0000

    #43: Add backend code for pagination

--- engage/www/trainings/submissions/index.py
+++ engage/www/trainings/submissions/index.py
@@ -7,6 +7,9 @@ from engage.utils import format_datetime_diff, require_login, require_trainer_ro
 @with_training
 @require_trainer_role
 def get_context(context, training):
+    pagination_params = get_pagination_parameters_from_form_dict(
+        frappe.form_dict)
+
     context.training = training
     context.submissions = frappe.get_all(
         "Practice Problem Latest Submission",
@@ -25,7 +28,8 @@ def get_context(context, training):
             "comment_count",
             "code",
         ],
-        order_by="submitted_at desc, modified desc")
+        order_by="submitted_at desc, modified desc",
+        **pagination_params)
 
     context.format_datetime_diff = format_datetime_diff
     context.get_submission_url = get_submission_url
@@ -33,3 +37,12 @@ def get_context(context, training):
 
 def get_submission_url(submission):
     return f"/trainings/{submission.training}/submissions/{submission.problem_set}/{submission.problem}?participant={submission.author}"
+
+
+def get_pagination_parameters_from_form_dict(d):
+    page_length = 60
+    page = int(d.page) if "page" in d else 1
+
+    start = (page - 1) * page_length
+
+    return dict(page_length=page_length, start=start)
