0a2204d619e8bb62bb3e9f1e3fcab3dec8d290f1
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-07-08 12:35:53 +0000

    closes #43: Add pagination for submissions

--- engage/www/trainings/submissions/index.py
+++ engage/www/trainings/submissions/index.py
@@ -1,20 +1,26 @@
+from math import ceil
 from urllib.parse import urlencode
 
 import frappe
 
 from engage.utils import format_datetime_diff, require_login, require_trainer_role, with_training
 
+PAGE_LENGTH = 5
+
 
 @require_login
 @with_training
 @require_trainer_role
 def get_context(context, training):
+    context.training = training
+
+    submission_filters = {"training": training.name},
     pagination_params = get_pagination_parameters_from_form_dict(
         frappe.form_dict)
 
-    context.training = training
+    submissions_doctype = "Practice Problem Latest Submission"
     context.submissions = frappe.get_all(
-        "Practice Problem Latest Submission",
+        submissions_doctype,
         fields=[
             "name",
             "author",
@@ -30,12 +36,22 @@ def get_context(context, training):
             "comment_count",
             "code",
         ],
-        filters={"training": training.name},
+        filters=submission_filters,
         order_by="submitted_at desc, modified desc",
         **pagination_params)
 
+    total_submissions = frappe.db.count(submissions_doctype,
+                                        filters=submission_filters)
+    context.total_pages = ceil(total_submissions / PAGE_LENGTH)
+    context.current_page = get_current_page(frappe.form_dict)
+
     context.format_datetime_diff = format_datetime_diff
+    context.get_submissions_url = get_submissions_url
     context.get_submission_url = get_submission_url
+
+
+def get_submissions_url(training_name, **querydict):
+    return f"/trainings/{training_name}/submissions?" + urlencode(querydict)
 
 
 def get_submission_url(submission):
@@ -45,9 +61,11 @@ def get_submission_url(submission):
 
 
 def get_pagination_parameters_from_form_dict(d):
-    page_length = 60
-    page = int(d.page) if "page" in d else 1
+    page = get_current_page(frappe.form_dict)
+    start = (page - 1) * PAGE_LENGTH
 
-    start = (page - 1) * page_length
+    return dict(page_length=PAGE_LENGTH, start=start)
 
-    return dict(page_length=page_length, start=start)
+
+def get_current_page(d):
+    return int(d.page) if "page" in d else 1
