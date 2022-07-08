05a535d56c2bf549a4774f698b3fd3b9d34b431e
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-07-08 11:28:06 +0000

    Fix #45: show all submissions, fix ordering

--- engage/www/trainings/submissions/index.py
+++ engage/www/trainings/submissions/index.py
@@ -8,19 +8,20 @@ from engage.utils import format_datetime_diff, require_login, require_trainer_ro
 @require_trainer_role
 def get_context(context, training):
     context.training = training
-    context.submissions = frappe.get_all("Practice Problem Latest Submission",
-                                         fields=[
-                                             "name",
-                                             "author",
-                                             "author_full_name",
-                                             "problem_set_title",
-                                             "problem",
-                                             "problem_title",
-                                             "submitted_at",
-                                             "test_outcome",
-                                             "comment_count",
-                                             "code",
-                                         ],
-                                         filters={"for_review": True},
-                                         order_by="submitted_at asc")
+    context.submissions = frappe.get_all(
+        "Practice Problem Latest Submission",
+        fields=[
+            "name",
+            "author",
+            "author_full_name",
+            "problem_set_title",
+            "problem",
+            "problem_title",
+            "submitted_at",
+            "modified",
+            "test_outcome",
+            "comment_count",
+            "code",
+        ],
+        order_by="submitted_at desc, modified desc")
     context.format_datetime_diff = format_datetime_diff
