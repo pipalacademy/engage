591057d7806a06e388447be2cfa7d50c5c3fe494
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-07-19 10:48:04 +0000

    fix #52: problem description on review page

--- engage/www/trainings/submissions/submission/index.py
+++ engage/www/trainings/submissions/submission/index.py
@@ -1,4 +1,4 @@
-# import frappe
+import frappe
 
 from engage.utils import get_prev_submission, get_next_submission, get_submission_url, format_datetime_diff, require_login, require_trainer_role, with_submission, with_training
 
@@ -10,6 +10,7 @@ from engage.utils import get_prev_submission, get_next_submission, get_submissio
 def get_context(context, training, submission):
     context.training = training
     context.submission = submission
+    context.problem = get_problem(submission.problem)
     context.format_datetime_diff = format_datetime_diff
 
     context.has_prev = get_prev_submission(submission) is not None
@@ -18,3 +19,7 @@ def get_context(context, training, submission):
     this_url = get_submission_url(submission.training, submission.name)
     context.prev_submission_url = this_url + "/prev"
     context.next_submission_url = this_url + "/next"
+
+
+def get_problem(problem_name):
+    return frappe.get_doc("Practice Problem", problem_name)
