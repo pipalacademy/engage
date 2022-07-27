53d8a374d7f2f9c8c4f4364b24af9a027e76fe96
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-07-27 05:56:50 +0000

    Replace custom escapeHTML with frappe.utils.escape_html

--- engage/www/trainings/submissions/index.js
+++ engage/www/trainings/submissions/index.js
@@ -93,15 +93,6 @@ function testsFailingIcon() {
     return `<i class="fa-solid fa-circle-xmark tests-failed-icon submission-icon"></i>`
 }
 
-function escapeHTML(unsafe) {
-    return unsafe
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
-}
-
 function getSubmissionCard(submission) {
     let submissionURL = `/trainings/${submission.training}/submissions/${submission.name}`
 
@@ -110,8 +101,8 @@ function getSubmissionCard(submission) {
     <div class="submission-card p-4 my-4">
         <div class="submission-card-header mb-4 d-flex justify-content-between">
             <div>
-                <div><strong>${escapeHTML(submission.author_full_name)}'s solution</strong></div>
-                <div class="text-muted">to ${escapeHTML(submission.problem_title)} in ${escapeHTML(submission.problem_set_title)}
+                <div><strong>${frappe.utils.escape_html(submission.author_full_name)}'s solution</strong></div>
+                <div class="text-muted">to ${frappe.utils.escape_html(submission.problem_title)} in ${frappe.utils.escape_html(submission.problem_set_title)}
                 </div>
             </div>
             <div class="d-flex mt-1 align-items-center submission-status">
@@ -122,7 +113,7 @@ function getSubmissionCard(submission) {
             </div>
         </div>
         <div class="submission-card-body">
-            <pre><code class="language-python">${escapeHTML(submission.code)}</code></pre>
+            <pre><code class="language-python">${frappe.utils.escape_html(submission.code)}</code></pre>
         </div>
         <div class="submission-card-footer d-flex justify-content-between align-items-center">
             <div>
