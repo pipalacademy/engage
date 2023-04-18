83271de19e69b2113f3a9389eeeb35e9e1aa6731
Kaustubh M <37668193+nikochiko@users.noreply.github.com>
2023-04-18 12:58:51 +0000

    remove autoname for problem set doctype

--- engage/engage/doctype/problem_set/problem_set.py
+++ engage/engage/doctype/problem_set/problem_set.py
@@ -5,5 +5,6 @@
 from frappe.model.document import Document
 
 class ProblemSet(Document):
-    def autoname(self):
-        self.name = self.title.lower().replace(" ", "-")
+    pass
+    # def autoname(self):
+    #     self.name = self.title.lower().replace(" ", "-")
