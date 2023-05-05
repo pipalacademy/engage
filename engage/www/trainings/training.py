e4994982209d67c39e2ab24788b573e45c749abd
Anand Chitipothu <anandology@gmail.com>
2023-05-05 12:34:17 +0530

    Improved the training page
    - Showed the status of the problem
    - Showed the comment count for every submission

--- engage/www/trainings/training.py
+++ engage/www/trainings/training.py
@@ -17,16 +17,6 @@ def get_context(context, training):
     if not member:
         return
 
-    user_submissions_list = frappe.get_all(
-        "Practice Problem Latest Submission",
-        filters={
-            "training": training.name,
-            "author": member.user
-        },
-        fields=["problem"],
-        page_length=1000)
-    user_submissions = {sub["problem"]: sub for sub in user_submissions_list}
-
     if training.refresh_problem_sets():
         training.save(ignore_permissions=True)
         frappe.db.commit()
@@ -36,7 +26,35 @@ def get_context(context, training):
     ]
 
     context.problem_sets = problem_sets
-    context.submissions = user_submissions
+    context.submissions = get_user_submissions(training.name, member.user)
+
+
+def get_user_submissions(training: str, user: str):
+    user_submissions_list = frappe.get_all(
+        "Practice Problem Latest Submission",
+        filters={
+            "training": training,
+            "author": user
+        },
+        fields=["name", "problem_set", "problem", "test_outcome"],
+        page_length=1000)
+
+    comment_counts = get_comment_counts(training, user)
+    for doc in user_submissions_list:
+        doc['comments'] = comment_counts.get(doc.name)
+    return {sub["problem"]: sub for sub in user_submissions_list}
+
+def get_comment_counts(training, user):
+    q = """
+    SELECT doc.name, count(r.name) as count
+    FROM `tabPractice Problem Latest Submission` as doc
+    LEFT OUTER JOIN `tabDiscussion Topic` as t ON  t.reference_doctype='Practice Problem Latest Submission' AND t.reference_docname=doc.name
+    LEFT OUTER JOIN `tabDiscussion Reply` as r ON r.topic=t.name
+    WHERE author="hima.varghese@strandls.com" and training="2023/strand-python-for-bioinformatics"
+    GROUP BY doc.name"""
+    values = {"user": user, "training": training}
+    rows = frappe.db.sql(q, values=values, as_dict=True)
+    return {row.name: row.count for row in rows}
 
 
 def time_is_between(val, lesser, greater):
