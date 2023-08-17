83799aa66813155ddc67ca7c96de7a81b80f8a16
Anand Chitipothu <anandology@gmail.com>
2023-08-17 17:30:39 +0530

    Added dependencies to setup.py instead of reading requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+#with open("requirements.txt") as f:
+#	install_requires = f.read().strip().split("\n")
+
+install_requires = [
+    "GitPython==3.1.27",
+    "pyyaml==5.4.1"
+]
 
 # get version from __version__ variable in engage/__init__.py
 from engage import __version__ as version
